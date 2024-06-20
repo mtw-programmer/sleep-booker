9556a716a27ac6bb04eb8aec7bc95f6de5342235
mtw-programmer <visnia58@gmail.com>
2024-06-20 23:21:23 +0200

    feat(reservations): setup pino logger

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -4,12 +4,24 @@ import { ReservationsController } from './reservations.controller';
 import { DatabaseModule } from '@app/common';
 import { ReservationsRepository } from './reservations.repository';
 import { ReservationDocument, ReservationSchema } from '../models/reservation.schema';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [DatabaseModule, DatabaseModule.forFeature([{
-    name: ReservationDocument.name,
-    schema: ReservationSchema
-  }])],
+      name: ReservationDocument.name,
+      schema: ReservationSchema
+    }]),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+          },
+        },
+      },
+    }),
+  ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
 })
