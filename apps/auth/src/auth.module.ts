85ad56ab25838e359a9160ef193719d190295f54
mtw-programmer <visnia58@gmail.com>
2024-06-23 00:32:06 +0200

    feat(auth): setup jwt

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -3,9 +3,18 @@ import { LoggerModule } from '@app/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from './users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, LoggerModule],
+  imports: [UsersModule, LoggerModule, JwtModule.registerAsync({
+    useFactory: (configService: ConfigService) => ({
+      secret: configService.get<string>('JWT_SECRET'),
+      signOptions: {
+        expiresIn: `${configService.get('JWT_EXPIRATION')}s`
+      },
+    }),
+  })],
   controllers: [AuthController],
   providers: [AuthService],
 })
