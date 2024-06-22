a407262ea40ce6609b4e09b7adf5352e404feee5
mtw-programmer <visnia58@gmail.com>
2024-06-23 00:48:15 +0200

    feat(apps): set port in .env

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -2,11 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { Logger } from 'nestjs-pino';
 import { AuthModule } from './auth.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
-  await app.listen(3001);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
