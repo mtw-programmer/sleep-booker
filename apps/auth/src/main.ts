5c08cd98a9e8b49d468d1641030c32affbf670fb
mtw-programmer <visnia58@gmail.com>
2024-06-23 21:31:10 +0200

    chore(auth): use cookie-parser

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,11 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { Logger } from 'nestjs-pino';
+import * as cookieParser from 'cookie-parser';
 import { AuthModule } from './auth.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
   const configService = app.get(ConfigService);
