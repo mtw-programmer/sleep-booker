6a61726e1d3cc4ce89db0991bf8dbf3ffc51d149
mtw-programmer <visnia58@gmail.com>
2024-06-23 15:22:18 +0200

    feat(auth): set up login in controller

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,9 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
+import { LocalAuthGuad } from './guards/local-auth.guard';
+import { CurrentUser } from './users/current-user.decorator';
+import { UserDocument } from './users/models/users.schema';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuad)
+  @Post('login')
+  async login (
+    @CurrentUser() user: UserDocument,
+    @Res({ passthrough: true }) response: Response
+  ) {
+    await this.authService.login(user, response);
+    response.send(user);
+  }
+
 
   @Get()
   getHello(): string {
