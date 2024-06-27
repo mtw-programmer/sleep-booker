966a9a5ae15c6848ae66f3adb6da5458188cb1c0
mtw-programmer <visnia58@gmail.com>
2024-06-27 17:02:00 +0200

    feat(auth): connect microservice

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -4,13 +4,16 @@ import { Logger } from 'nestjs-pino';
 import * as cookieParser from 'cookie-parser';
 import { AuthModule } from './auth.module';
 import { ConfigService } from '@nestjs/config';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+  app.connectMicroservice({ transport: Transport.TCP });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
   const configService = app.get(ConfigService);
+  await app.startAllMicroservices();
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
