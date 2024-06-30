f71d17f115aa53dc19edfa94c72a6b3f86cf03ac
mtw-programmer <visnia58@gmail.com>
2024-06-30 23:22:32 +0200

    feat(payments): setup dtos

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,5 +1,6 @@
 import { Type } from 'class-transformer';
-import { IsString, IsDate, IsNotEmpty } from 'class-validator';
+import { IsString, IsDate, IsNotEmpty, IsDefined, ValidateNested } from 'class-validator';
+import { CreateChargeDto } from '@app/common';
 
 export class CreateReservationDto {
   @IsDate()
@@ -17,4 +18,10 @@ export class CreateReservationDto {
   @IsString()
   @IsNotEmpty()
   invoiceId: string;
+
+  @IsDefined()
+  @IsNotEmpty()
+  @ValidateNested()
+  @Type(() => CreateChargeDto)
+  charge: CreateChargeDto
 }
---
+++ libs/common/src/dto/create-charge.dto.ts
@@ -0,0 +1,14 @@
+import { IsDefined, IsNotEmpty, IsNumber, ValidateNested } from 'class-validator';
+import { CardDto } from './card.dto';
+import { Type } from 'class-transformer';
+
+export class CreateChargeDto {
+  @IsDefined()
+  @IsNotEmpty()
+  @ValidateNested()
+  @Type(() => CardDto)
+  card: CardDto;
+
+  @IsNumber()
+  amount: number;
+}
